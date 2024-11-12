88a71bcbeab9ec3f400ec44d6b62d85023203013
CMM823062471 <carsse20@gmail.com>
2024-11-12 22:59:59 +0800

    test

---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "D:/Aaronhumm/person pro/vuepress/aaronhumm-cmm.github.io/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "D:/Aaronhumm/person pro/vuepress/aaronhumm-cmm.github.io/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "D:/Aaronhumm/person pro/vuepress/aaronhumm-cmm.github.io/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
